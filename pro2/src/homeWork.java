3e59137000d9351ac3f5bda131193eb1638a821f
dengshen <dengshenkk@gmail.com>
2018-03-26 19:45:53 +0800

    - fix   - 单词错误

--- pro2/src/homeWork.java
+++ pro2/src/homeWork.java
@@ -4,20 +4,21 @@ public class homeWork {
     /**
      * @deprecated 计算BMI 公式为 体重/(体重*身高)
      */
-    static float weith;
+    static float weigth;
     static float height;
     static private float result;
 
     public static void main(String[] args) {
         int i = 10;
+
         while (i >= 0) {
-            System.out.println("*****BMI计算器***Å**");
+            System.out.println("*****BMI计算器*****");
             System.out.println("请输入您的身高(m)");
             Scanner height = new Scanner(System.in);
             float h = height.nextFloat();
             System.out.println("请输入您的体重(kg)");
-            Scanner weith = new Scanner(System.in);
-            float w = weith.nextFloat();
+            Scanner weigth = new Scanner(System.in);
+            float w = weigth.nextFloat();
             result = w / (h * h);
             System.out.println("您的BMI值为" + result);
             i--;
